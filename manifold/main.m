675f463c0f9e32866cf2bf8c7d45d43e64b56681
Ortal Lavi <101092660+ortalagiv@users.noreply.github.com>
2024-01-18 11:36:26 +0200

    Update main.m - added an option for dataset generation

--- manifold/main.m
+++ manifold/main.m
@@ -5,11 +5,12 @@ addpath(genpath(pwd));
 cd ..;
 
 Nt = 12; Ns = 4; K = 16; Nrf = 2;
-S = load('channel.mat'); % of size [Ns, Nt, K]
+% dataset generation - you can also upload your own
+ch = randn(Ns, Nt, K); 
 
 for i = 1:K
     [u,s,v] = svd(ch(:,:,i)); 
     f_opt(:,:,i) = v(:,1:Ns);
 end
 
-[f_rf, f_bb, count] = MO_AltMin_wideband(f_opt, Nrf);
+[f_rf, f_bb, count] = MO_AltMin_wideband(f_opt);
